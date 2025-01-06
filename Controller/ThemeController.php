fa36c544c0e633175f6cfb037227fa6eb4d31e2f
Coderyounes <younesanas2021@gmail.com>
2025-01-06 21:08:45 +0100

    feat : #3 Creation d'un Controller contient les Crud de Théme

---
+++ Controller/ThemeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Younes\DriveLoc\Controller;
+
+trait ThemeController
+{
+    private $db;
+    private $tableTheme = "themes";
+    public function createTheme($instanceTheme)
+    {
+        $columns = implode(",", array_keys(get_object_vars($instanceTheme)));
+        $placeholders = ":" . implode(", :", array_keys(get_object_vars($instanceTheme)));
+        $sql = "INSERT INTO {$this->tableTheme} ({$columns}) VALUES ({$placeholders})";
+        $stmt = $this->db->prepare($sql);
+
+        foreach ($instanceTheme as $key => $value) {
+            $stmt->bindValue(":{$key}", $value);
+        }
+
+        return $stmt->execute();
+    }
+
+    public function updateTheme($id, $theme_name)
+    {
+        $query = "UPDATE $this->tableTheme SET theme_name = :theme_name WHERE theme_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':theme_name', $theme_name);
+        $stmt->execute();
+    }
+
+    public function deleteTheme($id)
+    {
+        $query = "UPDATE $this->tableTheme SET is_deleted = 1 WHERE theme_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function getAllThemes()
+    {
+        $sql = "SELECT * FROM {$this->tableTheme}";
+        $stmt = $this->db->prepare($sql);
+        if($stmt->execute()) {
+            return $stmt->fetchAll();
+        } else {
+            return null;
+        }
+    }
+}
