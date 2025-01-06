e6768b6ebaf96171eab48b41cdc202dbecb4eded
Coderyounes <younesanas2021@gmail.com>
2025-01-06 21:17:36 +0100

    feat : #7 Creation d'un Controller contient les Crud de tags

---
+++ Controller/TagsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Younes\DriveLoc\Controller;
+
+trait TagsController
+{
+    private $db;
+    private $tableTags = "tags";
+
+    public function createTag($instanceTag)
+    {
+        $columns = implode(",", array_keys(get_object_vars($instanceTag)));
+        $placeholders = ":" . implode(", :", array_keys(get_object_vars($instanceTag)));
+        $sql = "INSERT INTO {$this->tableTags} ({$columns}) VALUES ({$placeholders})";
+        $stmt = $this->db->prepare($sql);
+
+        foreach ($instanceTag as $key => $value) {
+            $stmt->bindValue(":{$key}", $value);
+        }
+
+        return $stmt->execute();
+    }
+
+    public function updateTag($id, $tag_name)
+    {
+        $query = "UPDATE $this->tableTags SET tag_name = :tag_name WHERE tag_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':tag_name', $tag_name);
+        $stmt->execute();
+    }
+
+    public function deleteTag($id)
+    {
+        $query = "UPDATE $this->tableTags SET is_deleted = 1 WHERE tag_id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function getAllTags()
+    {
+        $sql = "SELECT * FROM {$this->tableTags}";
+        $stmt = $this->db->prepare($sql);
+        if($stmt->execute()) {
+            return $stmt->fetchAll();
+        } else {
+            return null;
+        }
+    }
+
+}
